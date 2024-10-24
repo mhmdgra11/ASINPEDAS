dd78bdf7f57e6c4644ca168526272be5f69885da
mhmdgra11 <muhamad.agra70@smk.belajar.id>
2024-10-24 17:43:17 +0700

    aplikasi

---
+++ menu/menu.php
@@ -0,0 +1,35 @@
+      <div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="vendors/images/logoo.png" width="50" >
+			</a>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li>
+						<a href="index.php" class="dropdown-toggle no-arrow">
+							<span class="fa fa-pencil"></span><span class="mtext">Data Karyawan</span>
+						</a>
+					</li>
+                    <li>
+						<a href="?page=tampil-laporan" class="dropdown-toggle no-arrow">
+							<span class="fa fa-pencil"></span><span class="mtext">Laporan</span>
+						</a>
+					</li>
+
+                    <li>
+						<a href="?page=perjalanan-tampil" class="dropdown-toggle no-arrow">
+							<span class="fa fa-pencil"></span><span class="mtext">Perjalanan</span>
+						</a>
+					</li>
+                    <li>
+						<a href="logout.php" class="dropdown-toggle no-arrow">
+							<span class="fa fa-pencil"></span><span class="mtext">Keluar</span>
+						</a>
+					</li>
+    </ul>
+					
+			</div>
+		</div>
+	</div>
